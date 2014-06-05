df71ae0368d22ab4abb122cedddf56c21d564a1a
Martin Czuchra <martin.czuchra@googlemail.com>
2014-06-05 13:59:10 +0200

    Working once more!

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 # pull recent changes to dependencies and update theirs.
-git submodule foreach 'git pull && git submodule update --init'
+git submodule foreach 'git pull && git submodule update --init' 2> /dev/null
 
 # copy whispeer assets to www.
 cp -r ./dependencies/ssn/main/* ./www/
@@ -10,3 +10,6 @@ cp -r ./dependencies/ssn/main/* ./www/
 cp ./overrides/config.js ./www/assets/js/config.js
 cp ./overrides/generalWorkerInclude.js ./www/assets/js/cryptoWorker/generalWorkerInclude.js
 
+# override base href.
+sed 's/base href="\/"/base href="\."/' www/index.html > www/index-fixed.html
+mv www/index-fixed.html www/index.html
