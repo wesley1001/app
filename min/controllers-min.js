cef70ba60974d94ba2e9d31a3d5e6203bc18505d
Daniel Melchior <d.melchior@me.com>
2015-03-11 21:36:07 +0100

    initialise new ionic app

---
+++ min/controllers-min.js
@@ -0,0 +1,12 @@
+angular.module('starter.controllers', [])
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+});
+
