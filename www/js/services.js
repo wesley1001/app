4c6024331d8514483e16e2c6e0c3376c90d8869c
Daniel Melchior <d.melchior@me.com>
2015-03-12 15:20:17 +0100

    basic topic list functionality.

--- www/js/services.js
+++ www/js/services.js
@@ -4,36 +4,90 @@ angular.module('starter.services', [])
 	// Might use a resource here that returns a JSON array
 
 	// Some fake testing data
-	var chats = [{
-		id: 0,
-		name: 'Ben Sparrow',
-		lastText: 'You on your way?',
-		face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-	}, {
-		id: 1,
-		name: 'Max Lynx',
-		lastText: 'Hey, it\'s me',
-		face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-	}, {
-		id: 2,
-		name: 'Andrew Jostlin',
-		lastText: 'Did you get the ice cream?',
-		face: 'https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg'
-	}, {
-		id: 3,
-		name: 'Adam Bradleyson',
-		lastText: 'I should buy a boat',
-		face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-	}, {
-		id: 4,
-		name: 'Perry Governor',
-		lastText: 'Look at my mukluks!',
-		face: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg'
-	}];
+	var chats = [
+		{
+			id: 0,
+			type: 'groupChat',
+			title: 'Test',
+			lastMessage: 'Hey whats up?',
+			users: [
+				{
+					name: 'Matthew Reyes',
+					firstname: 'Matthew',
+					lastname: 'Reyes',
+					userimage: 'https://randomuser.me/api/portraits/med/men/40.jpg'
+				},
+				{
+					name: 'Irene Horton',
+					firstname: 'Irene',
+					lastname: 'Horton',
+					userimage: 'https://randomuser.me/api/portraits/med/women/54.jpg'
+				},
+				{
+					name: 'Tim Gray',
+					firstname: 'Tim',
+					lastname: 'Gray',
+					userimage: 'https://randomuser.me/api/portraits/med/men/43.jpg'
+				},
+				{
+					name: 'Janice Cunningham',
+					firstname: 'Janice',
+					lastname: 'Cunningham',
+					userimage: 'https://randomuser.me/api/portraits/med/women/5.jpg'
+				},
+				{
+					name: 'Alfredo Palmer',
+					firstname: 'Alfredo',
+					lastname: 'Palmer',
+					userimage: 'https://randomuser.me/api/portraits/med/men/73.jpg'
+				}
+			],
+			messages: [
+				{
+					user: {
+						name: 'Matthew Reyes',
+						firstname: 'Matthew',
+						lastname: 'Reyes',
+						userimage: 'https://randomuser.me/api/portraits/med/men/40.jpg'
+					},
+					messageText: 'Hey whats up?',
+					messageTime: '',
+					messageRead: [] // userids of users who read this message
+				}
+			]
+		},
+		{
+			id: 1,
+			type: 'peerChat',
+			title: 'Test',
+			lastMessage: 'Hey whats up?',
+			users: [
+				{
+					name: 'Janice Cunningham',
+					firstname: 'Janice',
+					lastname: 'Cunningham',
+					userimage: 'https://randomuser.me/api/portraits/med/women/5.jpg'
+				}
+			],
+			messages: [
+				{
+					user: {
+						name: 'Matthew Reyes',
+						firstname: 'Matthew',
+						lastname: 'Reyes',
+						userimage: 'https://randomuser.me/api/portraits/med/men/40.jpg'
+					},
+					messageText: 'Hey whats up?',
+					messageTime: '',
+					messageRead: [] // userids of users who read this message
+				}
+			]
+		}
+	];
 
 	return {
 		all: function() {
-		return chats;
+			return chats;
 		},
 		remove: function(chat) {
 			chats.splice(chats.indexOf(chat), 1);
