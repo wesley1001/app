acc1f0035f42141282b1f690515d64dd90de01f7
Daniel Melchior <d.melchior@me.com>
2015-07-13 12:17:51 +0200

    tabs nav for some parts of the app

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -2,11 +2,11 @@ define(["angular", "ionic", "controllers", "services"], function (angular) {
 	// Ionic Starter App
 
 	// angular.module is a global place for creating, registering and retrieving Angular modules
-	// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-	// the 2nd parameter is an array of 'requires'
-	// 'starter.services' is found in services.js
-	// 'starter.controllers' is found in controllers.js
-	return angular.module('whispeer', ['ionic', 'whispeer.controllers', 'whispeer.services'])
+	// "starter" is the name of this angular module example (also set in a <body> attribute in index.html)
+	// the 2nd parameter is an array of "requires"
+	// "starter.services" is found in services.js
+	// "starter.controllers" is found in controllers.js
+	return angular.module("whispeer", ["ionic", "whispeer.controllers", "whispeer.services"])
 		.run(function($ionicPlatform) {
 			$ionicPlatform.ready(function() {
 				// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -24,36 +24,50 @@ define(["angular", "ionic", "controllers", "services"], function (angular) {
 			// Ionic uses AngularUI Router which uses the concept of states
 			// Learn more here: https://github.com/angular-ui/ui-router
 			// Set up the various states which the app can be in.
-			// Each state's controller can be found in controllers.js
+			// Each state"s controller can be found in controllers.js
 			$stateProvider
 
 			// Each tab has its own nav history stack:
 
-			.state('login', {
-				url: '/login',
-				templateUrl: 'assets/views/pages/login.html',
-				controller: 'LoginCtrl'
+			.state("login", {
+				url: "/login",
+				templateUrl: "assets/views/pages/login.html",
+				controller: "LoginCtrl"
 			})
-			.state('chats', {
-				url: '/chats',
-				templateUrl: 'assets/views/pages/chats.html',
-				controller: 'ChatsCtrl'
+			.state("main", {
+				url: "/main",
+				templateUrl: "assets/views/pages/main.html"
 			})
-			.state('chat-detail', {
-				url: '/chat/:chatId',
-				templateUrl: 'assets/views/pages/chat-detail.html',
-				controller: 'ChatDetailCtrl'
+			.state("main.chats", {
+				url: "/chats",
+				templateUrl: "assets/views/pages/main/chats.html",
+				controller: "ChatsCtrl"
 			})
-			.state('newMessage', {
-				url: '/newMessage',
-				templateUrl: 'assets/views/pages/newMessage.html',
-				controller: 'NewMessageCtrl'
+			.state("main.friends", {
+				url: "/friends",
+				templateUrl: "assets/views/pages/main/friends.html",
+				controller: "FriendsCtrl"
+			})
+			.state("main.settings", {
+				url: "/settings",
+				templateUrl: "assets/views/pages/main/settings.html",
+				controller: "SettingsCtrl"
+			})
+			.state("chat-detail", {
+				url: "/chat/:chatId",
+				templateUrl: "assets/views/pages/chat-detail.html",
+				controller: "ChatDetailCtrl"
+			})
+			.state("newMessage", {
+				url: "/newMessage",
+				templateUrl: "assets/views/pages/newMessage.html",
+				controller: "NewMessageCtrl"
 			});
 
 			// if none of the above states are matched, use this as the fallback
-			$urlRouterProvider.otherwise('/login');
+			$urlRouterProvider.otherwise("/login");
 		})
-		.directive('savebutton', function () {
+		.directive("savebutton", function () {
 			return {
 				transclude: true,
 				scope:	{
@@ -86,7 +100,7 @@ define(["angular", "ionic", "controllers", "services"], function (angular) {
 				}
 			};
 		})
-		.directive('userimage', function () {
+		.directive("userimage", function () {
 			return {
 				transclude: false,
 				scope:	{
