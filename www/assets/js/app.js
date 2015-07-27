2f852080bc455c3acd152b64b24f67ab875a0296
Daniel Melchior <daniel@whispeer.de>
2015-07-27 12:46:56 +0200

    finally fix ios keyboard

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -45,6 +45,7 @@ define([
 				// for form inputs)
 				if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
 					cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+					cordova.plugins.Keyboard.disableScroll(true);
 				}
 				if (window.StatusBar) {
 					// org.apache.cordova.statusbar required
--- www/assets/js/controllers.js
+++ www/assets/js/controllers.js
@@ -64,6 +64,16 @@ define(["angular", "asset/state", "whispeerHelper"], function (angular, State, h
         "ssn.messageService",
         "ssn.errorService",
     function($scope, $stateParams, $timeout, $ionicScrollDelegate, messageService, errorService) {
+
+	    window.addEventListener("native.keyboardshow", function() {
+		   $ionicScrollDelegate.scrollBottom();
+		   $ionicScrollDelegate.resize();
+	    });
+
+	    window.addEventListener("native.keyboardhide", function() {
+		   $ionicScrollDelegate.resize();
+	    });
+
         var MINUTE = 60 * 1000;
 
         var topicLoadingState = new State();
