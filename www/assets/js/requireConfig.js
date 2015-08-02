50cb444eae190ca474cf44856d4e537de3ea72f1
Nils Kenneweg <nk@ovt.me>
2015-08-02 18:17:17 +0200

    add cordova to requirejs as an optional dependency.

--- www/assets/js/requireConfig.js
+++ www/assets/js/requireConfig.js
@@ -36,6 +36,38 @@ var baseElement = document.createElement("base");
 baseElement.setAttribute("href", base);
 document.getElementsByTagName("head")[0].appendChild(baseElement);
 
+define("optional", [], {
+    load : function (moduleName, parentRequire, onload){
+        "use strict";
+        var onLoadSuccess = function(moduleInstance){
+            // Module successfully loaded, call the onload callback so that
+            // requirejs can work its internal magic.
+            onload(moduleInstance);
+        };
+
+        var onLoadFailure = function(err){
+            // optional module failed to load.
+            var failedId = err.requireModules && err.requireModules[0];
+            console.warn("Could not load optional module: " + failedId);
+
+            // Undefine the module to cleanup internal stuff in requireJS
+            requirejs.undef(failedId);
+
+            // Now define the module instance as a simple empty object
+            // (NOTE: you can return any other value you want here)
+            define(failedId, [], function(){return {};});
+
+            // Now require the module make sure that requireJS thinks 
+            // that is it loaded. Since we've just defined it, requirejs 
+            // will not attempt to download any more script files and
+            // will just call the onLoadSuccess handler immediately
+            parentRequire([failedId], onLoadSuccess);
+        };
+
+        parentRequire([moduleName], onLoadSuccess, onLoadFailure);
+    }
+});
+
 requirejs.config({
     baseUrl: baseUrl,
     paths: {
@@ -59,9 +91,13 @@ requirejs.config({
         workerQueue: "../../lib/worker-queue.js/src/index",
         PromiseWorker: "../../lib/require-promise-worker.js/src/index",
         Dexie: "../../lib/dexie/dist/latest/Dexie",
-        debug: "../../lib/visionmedia-debug/dist/debug"
+        debug: "../../lib/visionmedia-debug/dist/debug",
+        ngCordova: "../../lib/ngCordova/dist/ng-cordova",
+        cordova: "../../cordova"
     },
     shim: {
+        cordova: { deps: ["ngCordova"] },
+        ngCordova: { deps: ["angular"] },
         angular : {exports : 'angular', deps: ['jquery']},
         angularAnimate : {deps: ['angular']},
         angularSanitize : {deps: ['angular']},
