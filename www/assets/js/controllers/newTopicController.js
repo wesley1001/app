e5e9e91289d4dfcbd7a233f2947b1e7f668c810f
Nils Kenneweg <nk@ovt.me>
2015-08-07 16:04:47 -0400

    send new message.

--- www/assets/js/controllers/newTopicController.js
+++ www/assets/js/controllers/newTopicController.js
@@ -1,6 +1,8 @@
-define(["step", "whispeerHelper", "controllers/controllerModule"], function (step, h, controllerModule) {
+define(["step", "whispeerHelper", "controllers/controllerModule", "asset/state"], function (step, h, controllerModule, State) {
 	"use strict";
-	controllerModule.controller("ssn.newTopicController", ["$scope", "ssn.friendsService", "ssn.userService", function($scope, friendsService, userService) {
+	controllerModule.controller("ssn.newTopicController", [
+		"$scope", "$state", "ssn.friendsService", "ssn.userService", "ssn.messageService", "ssn.errorService",
+		function($scope, $state, friendsService, userService, messageService, errorService) {
 		$scope.users = [];
 
 		step(function () {
@@ -9,5 +11,39 @@ define(["step", "whispeerHelper", "controllers/controllerModule"], function (ste
 		}, h.sF(function (result) {
 			$scope.users = result;
 		}));
+
+		var sendMessageState = new State();
+		$scope.sendMessageState = sendMessageState.data;
+
+		$scope.create = {
+			text: "",
+			selectedUsers: {},
+			send: function (text) {
+				var receiver = $scope.users.filter(function (u) {
+					return $scope.create.selectedUsers[u.id];
+				}).map(function (u) {
+					return u.id;
+				});
+
+				sendMessageState.pending();
+
+				if (text === "" || receiver.length === 0) {
+					sendMessageState.failed();
+					return;
+				}
+
+				step(function () {
+					messageService.sendNewTopic(receiver, text, [], this);
+				}, h.sF(function (id) {
+					$scope.create.text = "";
+					$scope.create.selectedUsers = {};
+					$scope.goToShow(id);
+				}), errorService.failOnError(sendMessageState));
+			}
+		};
+
+		$scope.goToShow = function (topicID) {
+			$state.go("chat-detail", { chatId: topicID });
+		};
 	}]);
 });
